84eed798cd2cc5c66f41935987bd18d1f1a24c99
fadianzkaba <108704916+fadianzkaba@users.noreply.github.com>
2024-11-09 08:24:01 +1100

    Removing the main function and add Cal Func

--- math/cmd/main.go
+++ math/cmd/main.go
@@ -1,14 +1,8 @@
 package calculator
 
 import (
-	"fmt"
 	"math/rand/v2"
 )
-
-func main() {
-	result := cal(1, 2)
-	fmt.Println(result)
-}
 
 func cal(val1 int, val2 int) int {
 	val3 := rand.IntN(6)
